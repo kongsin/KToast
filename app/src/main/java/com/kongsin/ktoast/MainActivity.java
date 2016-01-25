283441cf3adb4e97f6d78b0259022f46e065d26c
kongsin <pansansou@gmail.com>
2016-01-25 21:56:07 +0700

    add slide to remove dialog

--- app/src/main/java/com/kongsin/ktoast/MainActivity.java
+++ app/src/main/java/com/kongsin/ktoast/MainActivity.java
@@ -2,6 +2,10 @@ package com.kongsin.ktoast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.kongsin.customtoast.KToast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                KToast kToast = new KToast(MainActivity.this);
+                kToast.show(getString(R.string.app_name), "Test", null, null, KToast.DURATION_SHORT);
+            }
+        });
     }
 }
--- customtoast/src/main/java/com/kongsin/customtoast/KToast.java
+++ customtoast/src/main/java/com/kongsin/customtoast/KToast.java
@@ -5,7 +5,10 @@ import android.content.Context;
 import android.os.Handler;
 import android.support.annotation.ColorRes;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GestureDetectorCompat;
+import android.view.GestureDetector;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
@@ -16,18 +19,29 @@ import android.widget.TextView;
 /**
  * Created by Kongsin on 1/25/2016.
  */
-public class KToast {
+public class KToast implements GestureDetector.OnGestureListener {
 
-    public static final int SHORT = 2000;
-    public static final int LONG = 3500;
+    public static final int COLOR_TEAL = R.color.teal;
+    public static final int BLUE_GRAY = R.color.blue_gray;
+    public static final int DEEP_ORANGE = R.color.deep_orange;
+    public static final int GREEN = R.color.green;
+    public static final int GREY = R.color.grey;
+    public static final int YELLOW = R.color.yellow;
+    public static final int WHITE = R.color.white;
+    public static final int BLACK = R.color.black;
+
+    public static final int DURATION_SHORT = 2000;
+    public static final int DURATION_LONG = 3500;
     private TextView textTitle, textDetail;
     private Handler mHandler;
     private LinearLayout mContainer;
     private Animation animationIn, animationOut;
     private Context context;
+    private GestureDetectorCompat gestureDetectorCompat;
 
     public KToast(Context context) {
         this.context = context;
+        gestureDetectorCompat = new GestureDetectorCompat(context, this);
         ViewGroup container = (ViewGroup) ((Activity) context)
                 .findViewById(android.R.id.content);
         View v = ((Activity) context).getLayoutInflater().inflate(
@@ -39,6 +53,22 @@ public class KToast {
 
         mContainer = (LinearLayout) view.findViewById(R.id.container);
         mContainer.setVisibility(View.GONE);
+        mContainer.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                gestureDetectorCompat.onTouchEvent(event);
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (mContainer.getX() < (mContainer.getPivotX() / 1.5)) {
+                        mContainer.setX(0);
+                    } else {
+                        mContainer.startAnimation(animationOut);
+                        mHandler.removeCallbacks(runnable);
+                    }
+                }
+                return true;
+            }
+        });
+
         textTitle = (TextView) view.findViewById(R.id.title);
         textDetail = (TextView) view.findViewById(R.id.detail);
 
@@ -53,6 +83,7 @@ public class KToast {
             @Override
             public void onAnimationEnd(Animation animation) {
                 mContainer.setVisibility(View.GONE);
+                mContainer.setX(0);
             }
 
             @Override
@@ -100,4 +131,34 @@ public class KToast {
         }
     };
 
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return true;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        mContainer.setX(mContainer.getX() + (e2.getX() - e1.getX()));
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return true;
+    }
 }
